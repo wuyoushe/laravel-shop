389b7e8cd7119647d98509c18c5988c9d17cab5b
felton <wudadongfen@126.com>
2021-03-06 21:57:26 +0800

    '类目Seeder文件'

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersSeeder::class);
         // $this->call(UserSeeder::class);
         $this->call(UserAddressesSeeder::class);
+        $this->call(CategoriesSeeder::class);
         $this->call(ProductsSeeder::class);
         $this->call(OrdersSeeder::class);
     }
