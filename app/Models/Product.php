cf21f0c76f868571db1778655f02223d33140863
felton <wudadongfen@126.com>
2021-05-05 15:44:31 +0800

    '初始化众筹模型'

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,16 +9,29 @@ class Product extends Model
 {
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price'
+        'rating', 'sold_count', 'review_count', 'price', 'type'
     ];
 
     protected $casts = [
-        'on_sale'   => 'boolean',
+        'on_sale' => 'boolean',
     ];
+
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
 
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 
     public function category()
